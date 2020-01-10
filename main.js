3405e0b93deb35efb38c90ba1652f14e8c36e62b
LivingScripts <nilesh22.a67@gmail.com>
2020-01-10 20:15:46 +0530

    created binding for win

--- main.js
+++ main.js
@@ -1,78 +1,71 @@
-const { app, BrowserWindow, Menu } = require('electron')
+const { app, BrowserWindow, Menu } = require("electron");
 
-
-let win
+let win;
 
 function createWindow() {
-    win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile("src/display/index.html");
+
+  win.on("closed", () => {
+    win = null;
+  });
+
+  var menu = Menu.buildFromTemplate([
+    {
+      label: "Menu",
+      submenu: [
+        {
+          label: "Create Doc",
+          onclick() {
+            //EDIT TO CREATE DOC
+          }
+        },
+        {
+          label: "Browse Doc",
+          click() {
+            //  EDIT TO BROWSE DOCS
+          }
+        },
+        {
+          label: "Exit",
+          click() {
+            app.quit();
+          }
         }
-    })
-
-    win.loadFile('src/display/index.html')
-
-
-
-    win.on('closed', () => {
-        win = null
-    })
-
-
-    var menu = Menu.buildFromTemplate([{
-        label: 'Menu',
-        submenu: [
-            {
-                label: 'Create Doc',
-                onclick() {
-                    //EDIT TO CREATE DOC
-                }
-
-            },
-            {
-                label: 'Browse Doc',
-                click() {
-                    //  EDIT TO BROWSE DOCS
-                }
-            },
-            {
-                label: 'Exit',
-                click() {
-                    app.quit();
-                }
-            }
-        ]
+      ]
     },
     {
-        label: 'Settings',
-        submenu: [
-            {
-                label: 'Connection Settings'
-            },
-            {
-                label: 'About'
-            },
-        ]
+      label: "Settings",
+      submenu: [
+        {
+          label: "Connection Settings"
+        },
+        {
+          label: "About"
+        }
+      ]
     }
-
-    ])
-    Menu.setApplicationMenu(menu);
-
-
+  ]);
+  Menu.setApplicationMenu(menu);
 }
 
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
-})
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
 
-app.on('activate', () => {
-    if (win === null) {
-        createWindow()
-    }
-})
+app.on("activate", () => {
+  if (win === null) {
+    createWindow();
+  }
+});
